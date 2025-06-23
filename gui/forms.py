aec0b784f48d34d318edc37686b95b2ba678be1a
wario_is_here <117512622+warioishere@users.noreply.github.com>
2025-06-23 20:33:55 +0200

    fix enable emergency ep in forms.py

--- gui/forms.py
+++ gui/forms.py
@@ -121,6 +121,7 @@ updates_channel_codes = [
 class UpdateChannel(forms.Form):
     chan_id = forms.IntegerField(label='chan_id')
     target = forms.FloatField(label='target')
+    update_target = forms.ChoiceField(label='update_target', choices=updates_channel_codes)
 
 
 class UpdateClosing(forms.Form):
